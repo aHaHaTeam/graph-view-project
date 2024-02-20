a0897ce1cafc9d03e9d08be425e20cbd626c1b67
Sasha239 <alex.bogdanov05@mail.ru>
2024-02-20 12:32:20 +0300

    Change responsibilities of models and facades

--- wasm/facades/graph.go
+++ wasm/facades/graph.go
@@ -1,6 +1,32 @@
 package facades
 
+import (
+	"database/sql"
+	"graph-view-project/wasm/models"
+	"log"
+)
+
 type Graph struct {
-	nodes []Node
-	edges []Edge
+	model models.Graph
+	Nodes []Node
+	Edges []Edge
+}
+
+func (graph *Graph) GetId() int {
+	return graph.model.GetId()
+}
+
+func (graph *Graph) LoadNodes(db *sql.DB) {
+	rows, err := db.Query("SELECT * FROM Nodes WHERE id = $1", graph.GetId())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	graph.Nodes = make([]Node, 0)
+	for rows.Next() {
+		node := Node{}
+		err = rows.Scan(node.SetNodeId, node.SetNodeName, node.SetNodeData,
+			node.SetNodeColor, node.SetNodeShape, node.SetNodeSize)
+		graph.Nodes = append(graph.Nodes, node)
+	}
 }
--- wasm/models/graph.go
+++ wasm/models/graph.go
@@ -1,28 +1,11 @@
 package models
 
-import (
-	"database/sql"
-	"graph-view-project/wasm/facades"
-	"log"
-)
-
 type Graph struct {
 	id          int
 	name        string
 	description string
 }
 
-func (graph *Graph) GetNodes(db *sql.DB, nodes *[]facades.Node) {
-	rows, err := db.Query("SELECT * FROM Nodes WHERE id = $1", graph.id)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	*nodes = make([]facades.Node, 0)
-	for rows.Next() {
-		node := facades.Node{}
-		err = rows.Scan(node.SetNodeId, node.SetNodeName, node.SetNodeData,
-			node.SetNodeColor, node.SetNodeShape, node.SetNodeSize)
-		*nodes = append(*nodes, node)
-	}
+func (graph *Graph) GetId() int {
+	return graph.id
 }

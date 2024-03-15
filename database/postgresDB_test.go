24e17ea266cd267f4ef27e59ce613401984ad64c
Sasha239 <alex.bogdanov05@mail.ru>
2024-03-15 17:01:54 +0300

    Implement tests for database MockDB is ready to use

--- database/postgresDB_test.go
+++ database/postgresDB_test.go
@@ -1,15 +1,147 @@
 package database
 
 import (
+	_ "github.com/lib/pq"
 	"github.com/stretchr/testify/assert"
+	"graph-view-project/models"
+	"graph-view-project/server/utils"
+	"os"
 	"testing"
 )
 
-func TestConnection(t *testing.T) {
-	db := PostgresDB{}
-	err := db.Connect("test")
+var testedDB DataBase
+
+func Setup() {
+	testedDB = &MockDB{}
+	_ = testedDB.Connect("test")
+}
+
+func TearDown() {
+	_ = testedDB.Disconnect()
+}
+
+func CleanUp() {
+	TearDown()
+	Setup()
+}
+
+func TestConnectDisconnect(t *testing.T) {
+	t.Cleanup(CleanUp)
+	err := testedDB.Connect("test")
 	assert.Nil(t, err)
 
-	err = db.Disconnect()
+	err = testedDB.Disconnect()
 	assert.Nil(t, err)
+}
+
+func TestCreateUser(t *testing.T) {
+	t.Cleanup(CleanUp)
+
+	hash, _ := utils.GenerateHashPassword("PaSsWoRd")
+	user := models.User{Id: 1, Login: "user2", Email: "user@user", Password: hash, Graphs: make([]models.Graph, 0)}
+	err := testedDB.CreateUser(user)
+	assert.Nil(t, err)
+	err = testedDB.CreateUser(user)
+	assert.NotNil(t, err)
+}
+
+func TestCreateGraph(t *testing.T) {
+	t.Cleanup(CleanUp)
+
+	graph := models.Graph{Edges: make([]models.Edge, 0), Nodes: make([]models.Node, 0)}
+	graph.SetId(1)
+
+	err := testedDB.CreateGraph(graph)
+	assert.Nil(t, err)
+
+	err = testedDB.CreateGraph(graph)
+	assert.NotNil(t, err)
+}
+
+func TestCreateNode(t *testing.T) {
+	t.Cleanup(CleanUp)
+
+	node := models.Node{}
+	node.SetId(1)
+
+	err := testedDB.CreateNode(node)
+	assert.Nil(t, err)
+
+	err = testedDB.CreateNode(node)
+	assert.NotNil(t, err)
+}
+
+func TestCreateEdge(t *testing.T) {
+	t.Cleanup(CleanUp)
+
+	edge := models.Edge{}
+	edge.SetId(1)
+
+	err := testedDB.CreateEdge(edge)
+	assert.Nil(t, err)
+
+	err = testedDB.CreateEdge(edge)
+	assert.NotNil(t, err)
+}
+
+func TestUpdateUser(t *testing.T) {
+	t.Cleanup(CleanUp)
+
+	user := models.User{
+		Id:       1,
+		Login:    "1",
+		Email:    "1",
+		Password: "1",
+		Graphs:   make([]models.Graph, 0),
+	}
+	_ = testedDB.CreateUser(user)
+
+	graph := models.Graph{Nodes: nil, Edges: nil}
+	graph.SetId(239)
+	user.Graphs = append(user.Graphs, graph)
+
+	userInDb, _ := testedDB.GetUserByLogin("1")
+	assert.Equal(t, 0, len(userInDb.Graphs))
+
+	err := testedDB.UpdateUserByLogin(user.Login, user)
+	assert.Nil(t, err)
+
+	userInDb, _ = testedDB.GetUserByLogin("1")
+	assert.Equal(t, 1, len(userInDb.Graphs))
+
+	notExistingLogin := "fake"
+	err = testedDB.UpdateUserByLogin(notExistingLogin, user)
+	assert.NotNil(t, err)
+}
+
+func TestUpdateGraph(t *testing.T) {
+	t.Cleanup(CleanUp)
+
+	graph := models.Graph{
+		Nodes: make([]models.Node, 0),
+		Edges: make([]models.Edge, 0),
+	}
+	graph.SetId(1)
+
+	_ = testedDB.CreateGraph(graph)
+
+	node := models.Node{}
+	edge := models.Edge{}
+	graph.Nodes = append(graph.Nodes, node)
+	graph.Edges = append(graph.Edges, edge)
+
+	graphInDB, _ := testedDB.GetGraphById(1)
+	assert.Equal(t, 0, len(graphInDB.Nodes))
+
+	_ = testedDB.UpdateGraphById(graph.GetId(), graph)
+	graphInDB, _ = testedDB.GetGraphById(1)
+	assert.Equal(t, 1, len(graphInDB.Nodes))
+	assert.Equal(t, edge, graphInDB.Edges[0])
+}
+
+func TestMain(m *testing.M) {
+	Setup()
+	code := m.Run()
+	TearDown()
+	os.Exit(code)
 }
